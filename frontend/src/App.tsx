4e117086b37bb61f5bba413570fe42eb1389e099
Turkan Isayeva <turkantech@gmail.com>
2025-12-28 22:22:34 +0400

    Add frontend and backend folders

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import {
+  CheckIcon,
+  CreditCardIcon,
+  InfoIcon,
+  MailIcon,
+  SearchIcon,
+  StarIcon,
+} from "lucide-react";
+
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+
+export const App = () => {
+  return (
+    <div className="grid w-full max-w-sm gap-6">
+      <InputGroup>
+        <InputGroupInput placeholder="Search..." />
+        <InputGroupAddon>
+          <SearchIcon />
+        </InputGroupAddon>
+      </InputGroup>
+    </div>
+  );
+}
